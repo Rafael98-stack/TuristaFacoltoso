f3b1c977745c56dcdaf56d206e49c12513182574
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2025-01-03 15:38:00 +0100

    edit class prenotazione

--- src/Entities/Prenotazione.java
+++ src/Entities/Prenotazione.java
@@ -5,8 +5,52 @@ import java.time.LocalDateTime;
 public class Prenotazione {
     private static int idTot = 0;
     private int id;
-
     private LocalDateTime start;
     private LocalDateTime end;
+    private Abitazione abitazione;
+    private User user;
 
+    public Prenotazione(int id, LocalDateTime start, LocalDateTime end, Abitazione abitazione, User user) {
+        this.id = id;
+        this.start = start;
+        this.end = end;
+        this.abitazione = abitazione;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
+
+    public Abitazione getAbitazione() {
+        return abitazione;
+    }
+
+    public void setAbitazione(Abitazione abitazione) {
+        this.abitazione = abitazione;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
